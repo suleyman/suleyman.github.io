a1671fcad9bf21bb6c952cfb55bc135150fd5149
suleymandtr <slymnd28@gmail.com>
2021-10-24 22:58:06 +0300

    add: theme toggler

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,17 @@
 import NavItem from "./NavItem";
+import ThemeToggler from "./ThemeToggler";
 import { NextComponentType } from "next";
 
 const Header: NextComponentType = () => {
   return (
-    <header className="bg-red-500 text-white">
-      <div className="container mx-auto">
+    <header className="text-white max-w-4xl mx-auto  pt-8 pb-8">
+      <div className="container -ml-3 flex justify-between items-center">
         <nav>
           <NavItem href="/" text="Home" />
           <NavItem href="/about" text="About" />
           <NavItem href="/uses" text="Uses" />
         </nav>
+        <ThemeToggler />
       </div>
     </header>
   );
