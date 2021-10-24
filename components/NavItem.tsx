c9d8752c764a575c89dad30ff9125b95a891bed2
suleymandtr <slymnd28@gmail.com>
2021-10-24 22:58:36 +0300

    update: nav item, colors

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -10,8 +10,8 @@ const NavItem: ComponentType<Props> = (props) => {
   const { href, text } = props;
   return (
     <NextLink href={href}>
-      <a>
-        <span className="capsize">{text}</span>
+      <a className="font-normal text-gray-600 dark:text-gray-400 hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 transition-all">
+        <span>{text}</span>
       </a>
     </NextLink>
   );
